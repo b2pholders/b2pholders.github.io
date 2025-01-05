04fb30e1b78b4cf0fdc9f94fbee1a4420d9605ee
reyatwel <sysdev@gmx.com>
2025-01-06 07:09:22 +0800

    tested stylings for update profile

--- xch/bpl/jumi/profile_update_test.php
+++ xch/bpl/jumi/profile_update_test.php
@@ -139,6 +139,149 @@ function view_form($user_id, $admintype, $usertype): string
 
 	$str .= formCss();
 
+	$str .= <<<HTML
+	<!-- Name Section -->
+	<div class="row">
+		<div class="col-md-8 col-md-offset-1">
+			<form class="form-horizontal registration-form" role="form">
+				<fieldset>
+
+					<!-- Form Name -->
+					<legend>Personal Information Details</legend>
+
+					<!-- Text input-->
+					<div class="form-group">
+						<div class="col-sm-4">
+							<input type="text" name="fullname" placeholder="Full Name" class="form-control">
+						</div>					
+					</div>
+
+					<!-- Text input-->
+					<div class="form-group">
+						<div class="col-sm-4">
+							<select type="gender" placeholder="Gender" class="form-control">
+								<option value="female">Female</option>
+								<option value="male">Male</option>
+							</select>
+						</div>
+					</div>
+
+					<!-- Address Section -->
+					<!-- Form Name -->
+					<legend>Address Details</legend>
+					<!-- Text input-->
+					<div class="form-group">
+						<div class="col-sm-10">
+							<input type="text" name="addressLine1" placeholder="Address Line 1" class="form-control">
+						</div>
+					</div>
+					<!-- Text input-->
+					<div class="form-group">
+						<div class="col-sm-10">
+							<input type="text" name="addressLine2" placeholder="Address Line 2" class="form-control">
+						</div>
+					</div>
+					<!-- Text input-->
+					<div class="form-group">
+						<div class="col-sm-6">
+							<input type="text" name="city" placeholder="City" class="form-control">
+						</div>
+						<div class="col-sm-4">
+							<input type="text" name="state" placeholder="State" class="form-control">
+						</div>		
+					</div>
+
+					<div class="form-group">
+						<div class="col-sm-6">
+							<select type="gender" placeholder="Gender" class="form-control">
+								<option value="female">Female</option>
+								<option value="male">Male</option>
+							</select>
+						</div>						
+						<div class="col-sm-4">
+							<input type="text" name="postalCode" placeholder="Post Code" class="form-control">
+						</div>
+					</div>
+
+					<!-- Parent/Guadian Contact Section -->
+					<!-- Form Name -->
+					<legend>Parent/Guadian Information</legend>
+					<!-- Text input-->
+					<div class="form-group">
+						<div class="col-sm-4">
+							<input type="text" name="pFistName" placeholder="First Name" class="form-control">
+						</div>
+						<div class="col-sm-2">
+							<input type="text" name="pMiddleName" placeholder="Middle Name" class="form-control">
+						</div>
+						<div class="col-sm-4">
+							<input type="text" name="pLastName" placeholder="Last Name" class="form-control">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-2">
+							<select type="pContactMethod" placeholder="Contact Method" class="form-control">
+								<option>Contact Method</option>
+								<option value="phone">Phone</option>
+								<option value="text">Text</option>
+								<option value="email">Email</option>
+							</select>
+						</div>  
+						<div class="col-sm-4">
+							<input type="pPhoneNbr" placeholder="Phone Number" class="form-control">
+						</div>
+						<div class="col-sm-4">
+							<input type="email" name="pEmail" placeholder="Email" class="form-control">
+						</div>
+					</div>
+
+					<!-- Emergency Contact Section -->
+					<!-- Form Name -->
+					<legend>Emergency Contact Information</legend>
+					<!-- Text input-->
+					<div class="form-group">
+						<div class="col-sm-4">
+							<input type="text" name="eFistName" placeholder="First Name" class="form-control">
+						</div>
+						<div class="col-sm-2">
+							<input type="text" name="eMiddleName" placeholder="Middle Name" class="form-control">
+						</div>
+						<div class="col-sm-4">
+							<input type="text" name="eLastName" placeholder="Last Name" class="form-control">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-2">
+							<select type="pContactMethod" placeholder="Contact Method" class="form-control">
+								<option>Contact Method</option>
+								<option value="phone">Phone</option>
+								<option value="text">Text</option>
+								<option value="email">Email</option>
+							</select>
+						</div>  
+						<div class="col-sm-4">
+							<input type="text" name="ePhoneNbr" placeholder="Phone Number" class="form-control">
+						</div>
+						<div class="col-sm-4">
+							<input type="email" name="eEmail" placeholder="Email" class="form-control">
+						</div>
+					</div>
+
+					<!-- Command -->
+					<div class="form-group">
+						<div class="col-sm-5 col-sm-offset-1">
+							<div class="pull-right">
+								<button type="submit" class="btn btn-default">Cancel</button>
+								<button type="submit" class="btn btn-primary">Save</button>
+							</div>
+						</div>
+					</div>
+				</fieldset>
+			</form>
+		</div><!-- /.col-lg-12 -->
+	</div><!-- /.row -->
+	HTML;
+
 	return $str;
 }
 
